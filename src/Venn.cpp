126c08c4ae55237ccdccdb9a4a49e4489ad57694
Stochastic Telegraph <73925802+mahlenmorris@users.noreply.github.com>
2024-08-11 20:33:24 -0700

    Circle editing working well, and colors for circles

--- src/Venn.cpp
+++ src/Venn.cpp
@@ -87,17 +87,22 @@ struct Venn : Module {
 
     for (size_t channel = 0; channel < circles.size(); channel++) {
       const Circle& circle = circles.at(channel);
-      float x_distance = point.x - circle.x_center;
-      float y_distance = point.y - circle.y_center;
-      float distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+      if (circle.present) {
+        float x_distance = point.x - circle.x_center;
+        float y_distance = point.y - circle.y_center;
+        float distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 
-      if (distance > circle.radius) {
+        if (distance > circle.radius) {
+          outputs[DISTANCE_OUTPUT].setVoltage(0.0f, channel);
+          outputs[WITHIN_GATE_OUTPUT].setVoltage(0.0f, channel);
+        } else {
+          // We'll do linear distance scaling for now.
+          outputs[DISTANCE_OUTPUT].setVoltage((1 - distance / circle.radius) * 10, channel);
+          outputs[WITHIN_GATE_OUTPUT].setVoltage(10.0f, channel);
+        }
+      } else {
         outputs[DISTANCE_OUTPUT].setVoltage(0.0f, channel);
         outputs[WITHIN_GATE_OUTPUT].setVoltage(0.0f, channel);
-      } else {
-        // We'll do linear distance scaling for now.
-        outputs[DISTANCE_OUTPUT].setVoltage((1 - distance / circle.radius) * 10, channel);
-        outputs[WITHIN_GATE_OUTPUT].setVoltage(10.0f, channel);
       }
     }
   }
@@ -111,6 +116,18 @@ struct CircleDisplay : OpaqueWidget {
   Venn* module;
 
   CircleDisplay() {}
+
+  static constexpr int COLOR_COUNT = 7;
+  NVGcolor colors[COLOR_COUNT] = {
+    SCHEME_RED,
+    SCHEME_BLUE,
+    SCHEME_ORANGE,
+    SCHEME_PURPLE,
+    SCHEME_GREEN,
+    SCHEME_CYAN,
+    SCHEME_WHITE
+  };
+
 
   // Move point to current location if left clicked.
   void onButton(const ButtonEvent& e) override {
@@ -148,105 +165,129 @@ struct CircleDisplay : OpaqueWidget {
     }
   }
 
-void onSelectKey(const SelectKeyEvent& e) override {
-  // TODO: impose max/min size and position.
-  if (e.action == GLFW_PRESS || e.action == GLFW_REPEAT) {
-    if (module->current_circle >= 0) {  // i.e., there is a circle to edit.
-      // Editing the Circle.
-      // W - up
-      if (e.keyName == "w" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).y_center += 0.1;
-        e.consume(this);
+  void onSelectKey(const SelectKeyEvent& e) override {
+    // TODO: impose max/min size and position.
+    if (e.action == GLFW_PRESS || e.action == GLFW_REPEAT) {
+      if (module->current_circle >= 0) {  // i.e., there is a circle to edit.
+        // Editing the Circle.
+        // W - up
+        if (e.keyName == "w" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).y_center += 0.1;
+          e.consume(this);
+        }
+        // S - down
+        if (e.keyName == "s" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).y_center -= 0.1;
+          e.consume(this);
+        }
+        // A - left
+        if (e.keyName == "a" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).x_center -= 0.1;
+          e.consume(this);
+        }
+        // D - right
+        if (e.keyName == "d" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).x_center += 0.1;
+          e.consume(this);
+        }
+        // Q - smaller
+        if (e.keyName == "q" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).radius -= 0.1;
+          e.consume(this);
+        }
+        // E - bigger
+        if (e.keyName == "e" && (e.mods & RACK_MOD_CTRL) == 0) {
+          module->circles.at(module->current_circle).radius += 0.1;
+          e.consume(this);
+        }
       }
-      // S - down
-      if (e.keyName == "s" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).y_center -= 0.1;
-        e.consume(this);
-      }
-      // A - left
-      if (e.keyName == "a" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).x_center -= 0.1;
-        e.consume(this);
-      }
-      // D - right
-      if (e.keyName == "d" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).x_center += 0.1;
-        e.consume(this);
-      }
-      // Q - smaller
-      if (e.keyName == "q" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).radius -= 0.1;
-        e.consume(this);
-      }
-      // E - bigger
-      if (e.keyName == "e" && (e.mods & RACK_MOD_CTRL) == 0) {
-        module->circles.at(module->current_circle).radius += 0.1;
-        e.consume(this);
-      }
-    }
-    // Selecting which Circle.
-    // Z - previous
-    if (e.keyName == "z" && (e.mods & RACK_MOD_CTRL) == 0) {
-      // TODO: what if there are none? What happens above?
-      if (module->circles.size() > 0) {
-        for (int curr = module->current_circle - 1; curr != module->current_circle; curr--) {
-          if (curr < 0) {
-            curr = module->circles.size() - 1;
+      // Selecting which Circle.
+      // Z - previous
+      if (e.keyName == "z" && (e.mods & RACK_MOD_CTRL) == 0) {
+        // TODO: what if there are none? What happens above?
+        if (module->circles.size() > 0) {
+          for (int curr = module->current_circle - 1; curr != module->current_circle; curr--) {
+            if (curr < 0) {
+              curr = module->circles.size() - 1;
+            }
+            if (module->circles.at(curr).present) {
+              module->current_circle = curr;
+              break;
+            }
           }
-          if (module->circles.at(curr).present) {
+        }
+        e.consume(this);
+      }
+      // C - next
+      if (e.keyName == "c" && (e.mods & RACK_MOD_CTRL) == 0) {
+        // TODO: what if there are none? What happens above?
+        if (module->circles.size() > 0) {
+          for (int curr = module->current_circle + 1; curr != module->current_circle; curr++) {
+            if (curr >= (int) (module->circles.size())) {
+              curr = 0;
+            }
+            if (module->circles.at(curr).present) {
+              module->current_circle = curr;
+              break;
+            }
+          }
+        }
+        e.consume(this);
+      }
+
+      // Creation and deletion.
+      // F - add new one
+      if (e.keyName == "f" && (e.mods & RACK_MOD_CTRL) == 0) {
+        Circle circle;
+        // TODO: Center on where mouse is (if available).
+        circle.x_center = 5.0;  
+        circle.y_center = 5.0;
+        circle.radius = 1.0;
+        circle.present = true;
+        // What position should this be in?
+        // Let's start with the first non-present slot.
+        // Limit of 16 channels in a cable.
+        for (int curr = 0; curr < 16; curr++) {
+          if (curr == (int) module->circles.size()) {
+            module->circles.push_back(circle);
+            module->current_circle = curr;
+            break;
+          }
+          if (!(module->circles.at(curr).present)) {
+            module->circles.at(curr) = circle;
             module->current_circle = curr;
             break;
           }
         }
+        e.consume(this);
       }
-      e.consume(this);
-    }
-    // C - next
-    if (e.keyName == "c" && (e.mods & RACK_MOD_CTRL) == 0) {
-      // TODO: what if there are none? What happens above?
-      if (module->circles.size() > 0) {
-        for (int curr = module->current_circle + 1; curr != module->current_circle; curr++) {
-          if (curr >= (int) (module->circles.size())) {
-            curr = 0;
+
+      // x - delete current one.
+      if (e.keyName == "x" && (e.mods & RACK_MOD_CTRL) == 0) {
+        if (module->current_circle >= 0) {  // i.e., there is a circle to delete.
+          module->circles.at(module->current_circle).present = false;
+          // Move focus to next circle, if any.
+          bool found_next = false;
+          if (module->circles.size() > 0) {
+            for (int curr = module->current_circle + 1; curr != module->current_circle; curr++) {
+              if (curr >= (int) (module->circles.size())) {
+                curr = 0;
+              }
+              if (module->circles.at(curr).present) {
+                module->current_circle = curr;
+                found_next = true;
+                break;
+              }
+            }
           }
-          if (module->circles.at(curr).present) {
-            module->current_circle = curr;
-            break;
+          if (!found_next) {
+            module->current_circle = -1;  // Indicares no currently selected circle.
           }
         }
+        e.consume(this);
       }
-      e.consume(this);
     }
-    /*
-    // Ctrl+X
-    if (e.keyName == "x" && (e.mods & RACK_MOD_MASK) == RACK_MOD_CTRL) {
-      cutClipboard();
-      e.consume(this);
-    }
-    // Ctrl+C
-    if (e.keyName == "c" && (e.mods & RACK_MOD_MASK) == RACK_MOD_CTRL) {
-      copyClipboard();
-      e.consume(this);
-    }
-    // Ctrl+A
-    if (e.keyName == "a" && (e.mods & RACK_MOD_MASK) == RACK_MOD_CTRL) {
-      selectAll();
-      e.consume(this);
-    }
-    // Enter
-    if ((e.key == GLFW_KEY_ENTER || e.key == GLFW_KEY_KP_ENTER) && (e.mods & RACK_MOD_MASK) == 0) {
-      if (allow_text_entry) {
-        insertText("\n");
-      }
-      e.consume(this);
-    }
-    // Consume all printable keys unless Ctrl is held
-    if ((e.mods & RACK_MOD_CTRL) == 0 && e.keyName != "") {
-      e.consume(this);
-    }
-  */
   }
-}
 
   double nvg_x(float volt_x, double size) { 
     return volt_x * size / 10.0;
@@ -267,6 +308,12 @@ void onSelectKey(const SelectKeyEvent& e) override {
       // Assuming that we are always on a square-pixeled surface, with X and Y the same distances.
       double pixels_per_volt = bounding_box.x / 10.0;
 
+      // Background first.
+      nvgBeginPath(args.vg);
+      nvgRect(args.vg, 0.0, 0.0, bounding_box.x, bounding_box.y);
+      nvgFillColor(args.vg, SCHEME_DARK_GRAY);
+      nvgFill(args.vg);
+
       // The circles.
       // TODO: add number, name, and vary the color (same scheme as Memory?).
       int index = -1;
@@ -276,28 +323,30 @@ void onSelectKey(const SelectKeyEvent& e) override {
 
       for (const Circle& circle : module->circles) {
         index++;
-        nvgBeginPath(args.vg);
-        nvgCircle(args.vg, nvg_x(circle.x_center, bounding_box.x), nvg_y(circle.y_center, bounding_box.x),
-                pixels_per_volt * circle.radius);
-        nvgStrokeColor(args.vg, SCHEME_BLUE);
-        nvgStrokeWidth(args.vg, index == module->current_circle ? 2.0 : 1.0);
-        nvgStroke(args.vg);
+        if (circle.present) {
+          nvgBeginPath(args.vg);
+          nvgCircle(args.vg, nvg_x(circle.x_center, bounding_box.x), nvg_y(circle.y_center, bounding_box.x),
+                  pixels_per_volt * circle.radius);
+          nvgStrokeColor(args.vg, colors[index % COLOR_COUNT]);
+          nvgStrokeWidth(args.vg, index == module->current_circle ? 2.0 : 1.0);
+          nvgStroke(args.vg);
 
-        nvgFillColor(args.vg, color::BLACK);
-        nvgFontSize(args.vg, 13);
-        nvgFontFaceId(args.vg, font->handle);
-        //nvgTextLetterSpacing(args.vg, -2);
-        // Place in the center.
-        nvgText(args.vg, nvg_x(circle.x_center, bounding_box.x),
-                         nvg_y(circle.y_center, bounding_box.x),
-                         std::to_string(index + 1).c_str(), NULL);
+          nvgFillColor(args.vg, colors[index % COLOR_COUNT]);
+          nvgFontSize(args.vg, 13);
+          nvgFontFaceId(args.vg, font->handle);
+          //nvgTextLetterSpacing(args.vg, -2);
+          // Place in the center.
+          nvgText(args.vg, nvg_x(circle.x_center, bounding_box.x),
+                          nvg_y(circle.y_center, bounding_box.x),
+                          std::to_string(index + 1).c_str(), NULL);
+        }
       }
 
       // Draw the pointer (need a name for it).
       nvgBeginPath(args.vg);
       nvgCircle(args.vg, nvg_x(module->point.x, bounding_box.x), nvg_y(module->point.y, bounding_box.x),
               pixels_per_volt * 0.15);
-      nvgStrokeColor(args.vg, SCHEME_BLACK);
+      nvgStrokeColor(args.vg, SCHEME_WHITE);
       nvgStrokeWidth(args.vg, 1.0);
       nvgStroke(args.vg);
 
