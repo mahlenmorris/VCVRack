ca9a3bb495d344c72028d8e85ec13ee09850201a
Stochastic Telegraph <73925802+mahlenmorris@users.noreply.github.com>
2024-09-20 11:51:27 -0700

    Added square movement pattern
    And fixed the make.py program to have 10V be the peak amplitude. Removed trash from .svg file.

--- tools/make.py
+++ tools/make.py
@@ -13,7 +13,7 @@ FRAME_RATE = 1000.0
 #FRAME_RATE = 11025.0   # samples per second.
 #FRAME_RATE = 44100.0    # samples per second.
 #FRAME_RATE = 96000.0    # samples per second.
-AMPLITUDE = 16000.0     # multiplier for amplitude. Related to sampwidth in WriteFile.
+AMPLITUDE = 3276.8     # multiplier for amplitude. Makes +/-10.0 the max values. 
 LENGTH = 5             # Length of generated tones, in seconds
 DATA_SIZE = int(LENGTH * FRAME_RATE)
 
@@ -28,7 +28,7 @@ def WriteFile(samples_left, sample_right, fname):
     wav_file = wave.open(fname, "w")
     # give required parameters
     nchannels = 2
-    sampwidth = 2
+    sampwidth = 2  # 2^16 = 65536 possible values.
     framerate = int(FRAME_RATE)
     nframes = len(samples_left)
     comptype = "NONE"
@@ -39,38 +39,60 @@ def WriteFile(samples_left, sample_right, fname):
     # now write out the file ...
     for s, t in zip(samples_left, sample_right):
        # write the audio frames to file.
-       wav_file.writeframes(struct.pack('h', int(s * AMPLITUDE / 2)))
-       wav_file.writeframes(struct.pack('h', int(t * AMPLITUDE / 2)))
+       wav_file.writeframes(struct.pack('h', int(s * AMPLITUDE)))
+       wav_file.writeframes(struct.pack('h', int(t * AMPLITUDE)))
     wav_file.close()
 
 def Timestamp():
   return datetime.now().strftime('%b %d %H-%M-%S')
 
-for i in range(1):
-  # write the synthetic wave file to ...
-  fname = Timestamp() + '.wav'
+# write the synthetic wave file to ...
+fname = Timestamp() + '.wav'
 
-  print("Generating #%d..." % i)
-  samples_left = []
-  samples_right = []
-  for x in range(DATA_SIZE):
-    samples_left.append(math.sin(2 * math.pi * x / DATA_SIZE))
-    samples_right.append(math.cos(2 * math.pi * x / DATA_SIZE))
+print("Generating...")
+samples_left = []
+samples_right = []
+for x in range(DATA_SIZE):
+  # Makes a simple clockwise circle.
+  samples_left.append(2.5 * math.sin(2 * math.pi * x / DATA_SIZE))
+  samples_right.append(2.5 * math.cos(2 * math.pi * x / DATA_SIZE))
 
-  print('Writing...')
-  WriteFile(samples_left, samples_right, 'Waves\\' + fname)
-  print('File: %s\n' % fname)
-  print('\n\n')
-  print("%s written." % fname)
+
+print('Writing...')
+WriteFile(samples_left, samples_right, 'Waves\\' + fname)
+print('File: %s\n' % fname)
+print('\n\n')
+print("%s written." % fname)
 
 print('Done!')
 
 
 # Past examples:
-# These are the contents of the "for x in range(...)" loop
+# These are the contents of the "for x in range(...)" loops
+# for various shapes I wanted to make.
 
 '''
-    # Makes a simple clockwise circle.
-    samples_left.append(math.sin(2 * math.pi * x / DATA_SIZE))
-    samples_right.append(math.cos(2 * math.pi * x / DATA_SIZE))
+for x in range(DATA_SIZE):
+  # Makes a simple clockwise circle.
+  samples_left.append(2.5 * math.sin(2 * math.pi * x / DATA_SIZE))
+  samples_right.append(2.5 * math.cos(2 * math.pi * x / DATA_SIZE))
+'''
+
+'''
+# Makes a square traversal at 2.5V.
+quarter_way = DATA_SIZE / 4
+for x in range(DATA_SIZE):
+  match x // quarter_way:
+    case 0:
+      samples_left.append(-2.5 + 5.0 * x / quarter_way)
+      samples_right.append(2.5)
+    case 1:
+      samples_left.append(2.5)
+      samples_right.append(2.5 - 5.0 * (x - quarter_way) / quarter_way)
+    case 2:
+      samples_left.append(2.5 - 5.0 * (x - quarter_way * 2) / quarter_way)
+      samples_right.append(-2.5)
+    case 3:
+      samples_left.append(-2.5)
+      samples_right.append(-2.5 + 5.0 * (x - quarter_way * 3) / quarter_way)
 '''
