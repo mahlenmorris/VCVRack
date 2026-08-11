a67f7b5c987f8ad654e43e035983e420a375330f
Stochastic Telegraph <73925802+mahlenmorris@users.noreply.github.com>
2026-08-11 16:05:16 -0700

    Stop showing cppcheck issues with generated code.

--- run-static-check.sh
+++ run-static-check.sh
@@ -2,4 +2,4 @@
 
 # --suppress=unusedStructMember -- Not sure if Rack's cppcheck also runs with this or not.
 
-cppcheck --enable=all --suppress=missingIncludeSystem -I src/parser -I src/parser-venn src/  > local-static-checks.txt 2>&1
+cppcheck --enable=all --suppress=missingIncludeSystem --suppress=unusedStructMember --suppress=*:*parser.hh --suppress=*:*parser.cc --suppress=*:*scanner.cc -I src/parser -I src/parser-venn src/  > local-static-checks.txt 2>&1
