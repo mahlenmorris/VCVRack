0ba7f37f63cb0e6877f9643cbc4ceb788d0ae41e
Stochastic Telegraph <73925802+mahlenmorris@users.noreply.github.com>
2026-08-12 12:51:16 -0700

    Squaring the static analysis checks with what Rack is running.

--- run-static-check.sh
+++ run-static-check.sh
@@ -2,4 +2,20 @@
 
 # --suppress=unusedStructMember -- Not sure if Rack's cppcheck also runs with this or not.
 
-cppcheck --enable=all --suppress=missingIncludeSystem --suppress=unusedStructMember --suppress=*:*parser.hh --suppress=*:*parser.cc --suppress=*:*scanner.cc -I src/parser -I src/parser-venn src/  > local-static-checks.txt 2>&1
+cppcheck src \
+    -isrc/dep \
+    -isrc/tests \
+    --std=c++11 \
+    --max-configs=1 \
+    --enable=warning,performance,portability \
+    --suppress=*:*/dep/* \
+    --suppress=*:*/tests/* \
+    --suppress=missingIncludeSystem \
+    --suppress=unusedStructMember \
+    --suppress=*:*parser.hh \
+    --suppress=*:*parser.cc \
+    --suppress=*:*scanner.cc \
+    -I src/parser \
+    -I src/parser-venn \
+    -j $(nproc) \
+    -q > local-static-checks.txt 2>&1
